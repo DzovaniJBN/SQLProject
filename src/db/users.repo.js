e95facab408e6972cd9f78de922e0218d56cfe39
Jovan Filipov <jovan.filipov05@gmail.com>
2026-03-12 19:09:44 +0100

    Changed user files and applied RLS across

--- src/db/users.repo.js
+++ src/db/users.repo.js
@@ -1,69 +1,64 @@
-export async function insertUser(client, tenantId, email, role, passwordHash) {
+export async function insertUser(client, email, role, passwordHash) {
     return client.query(
         `
             INSERT INTO users (tenant_id, email, role, password_hash)
-            VALUES ($1, $2, $3, $4) RETURNING id, tenant_id, email, role, created_at
+            VALUES (current_setting('app.current_tenant')::uuid, $1, $2, $3)
+            RETURNING id, tenant_id, email, role, created_at
         `,
-        [tenantId, email, role, passwordHash]
+        [email, role, passwordHash]
     );
 }
 
-export async function getUserByEmail(client, tenantId, email) {
+export async function getUserByEmail(client, email) {
     return client.query(
         `
             SELECT id, tenant_id, email, role, created_at
             FROM users
-            WHERE tenant_id = $1
-              AND email = $2 LIMIT 1
+            WHERE email = $1 LIMIT 1
         `,
-        [tenantId, email]
+        [email]
     );
 }
 
 export async function getUserByEmailForAuth(client, email) {
     return client.query(
         `
-        SELECT id, tenant_id, email, role, password_hash
-        FROM users
-        WHERE email = $1
-        LIMIT 1
+            SELECT id, tenant_id, email, role, password_hash
+            FROM users
+            WHERE email = $1 LIMIT 1
         `,
         [email]
     );
 }
 
-export async function getUsersList(client, tenantId) {
+export async function getUsersList(client) {
     return client.query(
         `
-        SELECT id, tenant_id, email, role, created_at, password_hash
-        FROM users
-        WHERE tenant_id = $1
-        `,
-        [tenantId]
+            SELECT id, tenant_id, email, role, created_at, password_hash
+            FROM users
+        `
     )
 }
 
-export async function updateUser(client, tenantId, userId, email, role) {
+export async function updateUser(client, userId, email, role) {
     return client.query(
         `
             UPDATE users
-            SET email = $3,
-                role  = $4
-            WHERE id = $2
-              AND tenant_id = $1 RETURNING id, tenant_id, email, role, created_at
+            SET email = $2,
+                role  = $3
+            WHERE id = $1 RETURNING id, tenant_id, email, role, created_at
         `,
-        [tenantId, userId, email, role]
+        [userId, email, role]
     );
 }
 
-export async function deleteUser(client, tenantId, userId) {
+export async function deleteUser(client, userId) {
     return client.query(
         `
             DELETE
             FROM users
-            WHERE id = $2
-              AND tenant_id = $1 RETURNING id, tenant_id, email, role, created_at
+            WHERE id = $1 RETURNING id, tenant_id, email, role, created_at
         `,
-        [tenantId, userId]
+        [userId]
     );
 }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,55 +2,42 @@ import * as userRepo from '../db/users.repo.js';
 import bcrypt from 'bcrypt';
 import {BadRequestError} from '../errors/badRequest.error.js';
 
-export const insertUser = async (client, tenantId, email, password) => {
+export const insertUser = async (client, email, password) => {
     const hash = await bcrypt.hash(password, 10);
 
     return userRepo.insertUser(
         client,
-        tenantId,
         email,
         "member",
         hash
     );
 };
 
-export const getUserByEmail = async (client, tenantId, email) => {
-    const result = await userRepo.getUserByEmail(client, tenantId, email);
+export const getUserByEmail = async (client, email) => {
+    const result = await userRepo.getUserByEmail(client, email);
     return result;
 }
 
-export const getUsersList = async (client, tenantId) => {
-    if (!tenantId) {
-        throw new BadRequestError('Tenant ID is required');
-    }
-
-    const result = await userRepo.getUsersList(client, tenantId);
+export const getUsersList = async (client) => {
+    const result = await userRepo.getUsersList(client);
     return result;
 }
 
-export const updateUser = async (client, tenantId, userId, email, role) => {
-    if (!tenantId) {
-        throw new BadRequestError('Tenant ID is required');
-    }
-    
+export const updateUser = async (client, userId, email, role) => {
     if (!userId) {
         throw new BadRequestError('User ID is required');
     }
 
-    const result = await userRepo.updateUser(client, tenantId, userId, email, role);
+    const result = await userRepo.updateUser(client, userId, email, role);
     return result;
 }
 
-export const deleteUser = async (client, tenantId, userId) => {
-    if (!tenantId) {
-        throw new BadRequestError('Tenant ID is required');
-    }
-
+export const deleteUser = async (client, userId) => {
     if (!userId) {
         throw new BadRequestError('User ID is required');
     }
 
-    const result = await userRepo.deleteUser(client, tenantId, userId);
+    const result = await userRepo.deleteUser(client, userId);
     return result;
 }
 
